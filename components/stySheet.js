d1470f6f65009d99d268c9413665dac557900dcf
chinnawich <hotlnw159@gmail.com>
2022-06-22 10:21:13 +0700

    commit Recap

---
+++ components/stySheet.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native'
+
+const  styles = StyleSheet.create({
+    textBox: {
+        position: 'absolute',
+        top: 10,
+        shadowColor: '#eee',
+        backgroundColor: "#fff",
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        padding: 10,
+        borderRadius: 10,
+        width: 250,
+        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+        
+    },
+    textColor: {
+      color: "#070f6e",
+      fontSize: 30,
+      fontWeight: 'bold',
+    }
+  })
+
+
+  export default styles
+ 
